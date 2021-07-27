0669fc8a51e932d327161eb622a821489229cc7a
Obakeng <obakeng@users-MacBook-Air.local>
2021-07-27 14:55:33 +0200

    Added Travis CI yml

--- src/main/java/com/wecode/bookstore/controller/BookController.java
+++ src/main/java/com/wecode/bookstore/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.wecode.bookstore.controller;
 
 import com.wecode.bookstore.dto.BookDto;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
